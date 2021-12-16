894c642db3a8512da1055b7dfb4d606c1279b9e2
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2021-12-16 03:01:54 -0300

    faunaDB config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,6 +17,19 @@ export default NextAuth({
           },
       },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const e_mail = user.email;
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email: e_mail } }
+        )
+      )
+
+      return true
+    },
+  }
 })
